5c68d9ce69dfc169bb7881a4d63f88c524eda996
rodrigo <rodrigo_l321@hotmail.com>
2021-02-26 14:34:22 -0500

    Servicio usuarios-commons

---
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/commons/models/entity/Role.java
@@ -0,0 +1,30 @@
+package com.formacionbdi.springboot.app.usuarios.commons.models.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true,length = 30)
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
